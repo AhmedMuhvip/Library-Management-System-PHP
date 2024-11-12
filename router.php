29ad7034e055b97209f91027eb5f7ea91d1d8e94
Ahmed Muhammed <ahmedmuhammed0031@gmail.com>
2024-11-12 11:04:33 +0300

    feat: implement router and 404 Page

---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+$routes = [
+        '/'       => 'views/login_view.php',
+        '/signup' => 'views/signup_view.php',
+];
+
+
+function route($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+route($uri, $routes);
